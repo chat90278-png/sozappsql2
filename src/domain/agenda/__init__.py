fcf561e6b49d9c352d1769dc7d9020e7f4604d94
chat90278-png <chat90278@gmail.com>
2026-07-11 09:55:22 +0300

    feat: export agenda condition engine

--- src/domain/agenda/__init__.py
+++ src/domain/agenda/__init__.py
@@ -13,6 +13,7 @@ from src.domain.agenda.deadline_stage import (
     deadline_stage_version,
 )
 from src.domain.agenda.keys import build_agenda_key
+from src.domain.agenda.lifecycle import AgendaLifecycleDecision, AgendaLifecycleEngine
 from src.domain.agenda.models import (
     AgendaContext,
     AgendaItem,
@@ -21,18 +22,30 @@ from src.domain.agenda.models import (
     AgendaResult,
 )
 from src.domain.agenda.priority import AGENDA_SEVERITY_RANK, severity_rank
+from src.domain.agenda.providers import (
+    AgendaProvider,
+    DeadlineAgendaProvider,
+    UnknownDateAgendaProvider,
+)
+from src.domain.agenda.source_models import AgendaCalendarSource
 
 __all__ = [
     "AGENDA_SEVERITY_RANK",
+    "AgendaCalendarSource",
     "AgendaContext",
     "AgendaItem",
     "AgendaItemState",
+    "AgendaLifecycleDecision",
+    "AgendaLifecycleEngine",
     "AgendaLifecycleType",
     "AgendaPresentationProfile",
     "AgendaPresentationProfileCode",
+    "AgendaProvider",
     "AgendaResult",
     "AgendaSeverity",
+    "DeadlineAgendaProvider",
     "DeadlineStage",
+    "UnknownDateAgendaProvider",
     "build_agenda_key",
     "deadline_stage_for_remaining_days",
     "deadline_stage_rank",
