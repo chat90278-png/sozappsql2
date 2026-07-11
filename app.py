4fcb170ccaa6b32bbd088251f8b3235b6f4f1686
chat90278-png <chat90278@gmail.com>
2026-07-11 13:44:37 +0300

    Install SD timing dialog extension

--- app.py
+++ app.py
@@ -18,11 +18,13 @@ from src.ui.contract.multiplatform_context_refresh import install_multiplatform_
 from src.ui.corner_menu_runtime_fix import install_corner_menu_runtime_fix
 from src.ui.main_page_identity_runtime_fix import install_main_page_identity_runtime_fix
 from src.ui.dialogs.contract_edit_timing_runtime_fix import install_contract_edit_timing_fix
+from src.ui.dialogs.sd_edit_timing_runtime_fix import install_sd_edit_timing_fix
 
 install_multiplatform_context_refresh_fix()
 install_corner_menu_runtime_fix()
 install_main_page_identity_runtime_fix()
 install_contract_edit_timing_fix()
+install_sd_edit_timing_fix()
 
 from src.ui.main_window import (
     app_icon_path,
