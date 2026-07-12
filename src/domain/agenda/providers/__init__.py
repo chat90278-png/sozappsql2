525ae96c0efb015b3d75a3e47e3e3a3b5f674e70
chat90278-png <chat90278@gmail.com>
2026-07-12 20:28:48 +0300

    feat: export returned share agenda provider

--- src/domain/agenda/providers/__init__.py
+++ src/domain/agenda/providers/__init__.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from src.domain.agenda.providers.base import AgendaProvider
 from src.domain.agenda.providers.deadline import DeadlineAgendaProvider
+from src.domain.agenda.providers.returned_share import ReturnedShareAgendaProvider
 from src.domain.agenda.providers.unknown_date import UnknownDateAgendaProvider
 
 __all__ = [
     "AgendaProvider",
     "DeadlineAgendaProvider",
+    "ReturnedShareAgendaProvider",
     "UnknownDateAgendaProvider",
 ]
