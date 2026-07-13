ad41278481dc2ddb3c9b998d41de8ce74e2749b5
chat90278-png <chat90278@gmail.com>
2026-07-13 10:55:54 +0300

    Add agenda document lock provider

--- src/domain/agenda/providers/__init__.py
+++ src/domain/agenda/providers/__init__.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 from src.domain.agenda.providers.base import AgendaProvider
 from src.domain.agenda.providers.deadline import DeadlineAgendaProvider
+from src.domain.agenda.providers.document_lock import DocumentLockAgendaProvider
 from src.domain.agenda.providers.returned_share import ReturnedShareAgendaProvider
 from src.domain.agenda.providers.unknown_date import UnknownDateAgendaProvider
 
 __all__ = [
     "AgendaProvider",
     "DeadlineAgendaProvider",
+    "DocumentLockAgendaProvider",
     "ReturnedShareAgendaProvider",
     "UnknownDateAgendaProvider",
 ]
